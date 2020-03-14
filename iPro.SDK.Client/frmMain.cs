4d1dbf45a2c4fcbbdef18965713668ab90583a03
Rulee Chen <rulee@live.cn>
2020-03-14 22:06:39 +0800

    connect with isolated helpers

--- iPro.SDK.Client/frmMain.cs
+++ iPro.SDK.Client/frmMain.cs
@@ -1,5 +1,7 @@
 ﻿using DotNetOpenAuth.OAuth2;
 using EBA.Ex;
+using iPro.SDK.Client.Helpers;
+using iPro.SDK.Client.Models;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -9,8 +11,6 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Mime;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -48,90 +48,13 @@ namespace iPro.SDK.Client
             txtPropertyRatesApi.Text = txtPropertyRatesApi.Text.Replace("{date}", now.Date.ToString("yyyy-MM-dd"));
         }
 
-        public class ParsedResult
-        {
-            public string Message { get; set; }
-            public bool Success { get; set; }
-        }
-
-        public class JsonResponse
-        {
-            public bool Success { get; set; }
-        }
-
-        private static string HandleWebException(WebException ex)
-        {
-            var sb = new StringBuilder();
-
-            using (var response = ex.Response)
-            {
-                try
-                {
-                    var httpResponse = (HttpWebResponse)response;
-
-                    sb.AppendLine("-------------Message-------------");
-                    sb.AppendLine($"StatusCode: {httpResponse.StatusCode}");
-                    sb.AppendLine($"Status: {httpResponse.StatusDescription}");
-
-                    using (var data = response.GetResponseStream())
-                    {
-                        var text = new StreamReader(data).ReadToEnd();
-                        sb.AppendLine(text);
-                    }
-                }
-                catch
-                {
-                }
-            }
-
-            sb.AppendLine(string.Empty);
-            sb.AppendLine("-------------Error-------------");
-            sb.AppendLine(ex.ToString());
-            sb.AppendLine(string.Empty);
-
-            return sb.ToString();
-        }
-
         private async Task<ParsedResult> HandleRequestState(Func<Task<ParsedResult>> func)
         {
             var stopwatch = new Stopwatch();
             stopwatch.Start();
             lblTimeCost.Text = @"Waiting for server response...";
 
-            ParsedResult result = null;
-            try
-            {
-                result = await func();
-            }
-            catch (WebException ex)
-            {
-                result = new ParsedResult
-                {
-                    Success = false,
-                    Message = HandleWebException(ex),
-                };
-            }
-            catch (Exception ex)
-            {
-                if (ex.InnerException is WebException)
-                {
-                    var webException = (WebException)ex.InnerException;
-                    result = new ParsedResult
-                    {
-                        Success = false,
-                        Message = HandleWebException(webException),
-                    };
-                }
-                else
-                {
-                    result = new ParsedResult
-                    {
-                        Success = false,
-                        Message = (ex.InnerException ?? ex).ToString(),
-                    };
-                }
-            }
-
+            var result = await RequestHelpers.HandleRequestState(func);
             if (result != null)
             {
                 outputTextBox.Text = result.Message;
@@ -161,68 +84,7 @@ namespace iPro.SDK.Client
 
             var client = new WebServerClient(serverDescription, oauth2ClientIdTextBox.Text, oauth2ClientSecretTextBox.Text);
 
-            return (client);
-        }
-
-        private static async Task<ParsedResult> ParseResponse(HttpWebRequest httpRequest)
-        {
-            var response = await httpRequest.GetResponseAsync();
-            var message = string.Format("Status Code: {0}" + Environment.NewLine, (int)((HttpWebResponse)response).StatusCode);
-            var success = false;
-
-            var contentDispositionHeader = response.Headers["Content-Disposition"];
-            if (!string.IsNullOrWhiteSpace(contentDispositionHeader))
-            {
-                success = true;
-                message += contentDispositionHeader;
-
-                var contentDisposition = new ContentDisposition(contentDispositionHeader);
-                var extName = response.ContentType == MediaTypeNames.Application.Pdf ? ".pdf" : Path.GetExtension(contentDisposition.FileName);
-
-                using (var dialog = new SaveFileDialog())
-                {
-                    dialog.Filter = "All files (*.*)|*.*";
-                    dialog.FilterIndex = 1;
-                    dialog.RestoreDirectory = true;
-                    dialog.DefaultExt = extName;
-                    dialog.FileName = contentDisposition.FileName;
-
-                    if (dialog.ShowDialog() == DialogResult.OK)
-                    {
-                        using (var stream = dialog.OpenFile())
-                        {
-                            var res = response.GetResponseStream();
-                            res.CopyTo(stream);
-                        }
-                    }
-                }
-            }
-            else
-            {
-                var reader = new StreamReader(response.GetResponseStream());
-                var responseText = reader.ReadToEnd();
-
-                if (response.ContentType == "application/json")
-                {
-                    var responseObj = JsonConvert.DeserializeObject<JsonResponse>(responseText);
-                    success = responseObj.Success;
-
-                    var parsedObj = JsonConvert.DeserializeObject<dynamic>(responseText);
-                    var formattedJson = JsonConvert.SerializeObject(parsedObj, Formatting.Indented);
-                    message += formattedJson;
-                }
-                else
-                {
-                    success = true;
-                    message += responseText;
-                }
-            }
-
-            return new ParsedResult
-            {
-                Success = success,
-                Message = message,
-            };
+            return client;
         }
 
         private void GetAccessToken()
@@ -251,7 +113,7 @@ namespace iPro.SDK.Client
                     httpRequest.IfModifiedSince = Convert.ToDateTime(txtIfModifiedSince.Text);
                 }
 
-                var result = await ParseResponse(httpRequest);
+                var result = await RequestHelpers.ParseResponse(httpRequest);
                 return result;
             });
         }
@@ -275,7 +137,7 @@ namespace iPro.SDK.Client
                     postStream.Close();
                 }
 
-                var result = await ParseResponse(httpRequest);
+                var result = await RequestHelpers.ParseResponse(httpRequest);
                 return result;
             });
         }
